66dbc3f0e96e1add3148a6aaa71879f60e909e34
VeroMiyashiro <138074991+VeroMiyashiro@users.noreply.github.com>
2023-07-17 19:04:35 -0300

    correcciones.py

--- proyecto.py
+++ proyecto.py
@@ -1,34 +1,65 @@
 from random import choice
-palabras = ['celular', 'camello', 'python','bote','pizarron']
-letras_incorrectas =[]
+
+palabras = ['celular', 'camello', 'python', 'bote', 'pizarron']
+letras_incorrectas = []
 letras_correctas = []
 intentos = 5
 aciertos = 0
 juego_terminado = False
 
 def elegir_palabra(lista_palabras):
-
     palabra_elegida = choice(lista_palabras)
-    letras_unicas =len(set(palabra_elegida))
+    letras_unicas = len(set(palabra_elegida))
     return palabra_elegida, letras_unicas
 
-palabra = elegir_palabra(palabras)
+palabra_elegida, letras_unicas = elegir_palabra(palabras)
 
 def mostrar_nuevo_tablero(palabra_elegida):
-    lista_oculta =[]
+    lista_oculta = []
     for l in palabra_elegida:
         if l in letras_correctas:
-            lista_oculta.append(1)
+            lista_oculta.append(l)
         else:
             lista_oculta.append('__')
     print(lista_oculta)
 
-
 def pedir_letra():
-    abecedario ='abcdfghikalmnñopqrstuvwxyz'
-    letra=input("Ingrease una letra al juego:").lower()
-    if len(letra) !=1 or letra not in abbecedario: 
+    abecedario = 'abcdefghijklmnopqrstuvwxyz'
+    letra = input("Ingresa una letra al juego: ").lower()
+    if len(letra) != 1 or letra not in abecedario:
         print("Ingresa solo letras.")
         return pedir_letra()
-    return letra 
-#no te olvides de gurdar los cambios, sino no carga
+    return letra
+
+def verificar_letra(letra):
+    if letra in palabra_elegida:
+        letras_correctas.append(letra)
+        return True
+    else:
+        letras_incorrectas.append(letra)
+        return False
+
+while not juego_terminado:
+    mostrar_nuevo_tablero(palabra_elegida)
+    print("Letras incorrectas:", letras_incorrectas)
+    print("Intentos restantes:", intentos)
+
+    letra = pedir_letra()
+
+    if verificar_letra(letra):
+        aciertos += 1
+        if aciertos == letras_unicas:
+            print("¡Felicitaciones! Has adivinado la palabra:", palabra_elegida)
+            juego_terminado = True
+            break
+    else:
+        intentos -= 1
+        if intentos == 0:
+            print("¡Uy! Te quedaste sin intentos. La palabra era:", palabra_elegida)
+            juego_terminado = True
+            break
+
+
+
+if juego_terminado:
+    print("¡Fin del juego!")o
