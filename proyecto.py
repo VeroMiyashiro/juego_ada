fc6628e9de84f9db670043af3a06605798a302b3
VeroMiyashiro <138074991+VeroMiyashiro@users.noreply.github.com>
2023-07-17 15:17:17 -0300

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -7,12 +7,12 @@ aciertos = 0
 juego_terminado = False
 
 def elegir_palabra(lista_palabras):
-    palabra = lista_palabras(lista_palabras):
-    palabra ekegida = choice(lista_palabras):
-    letras unicas =len(set(palabra_elegida))
+
+    palabra_elegida = choice(lista_palabras)
+    letras_unicas =len(set(palabra_elegida))
     return palabra_elegida, letras_unicas
 
-palabra = elegir_palabr(palabras)
+palabra = elegir_palabra(palabras)
 
 def mostrar_nuevo_tablero(palabra_elegida):
     lista_oculta =[]
@@ -21,4 +21,14 @@ def mostrar_nuevo_tablero(palabra_elegida):
             lista_oculta.append(1)
         else:
             lista_oculta.append('__')
-print(lista_oculta)
+    print(lista_oculta)
+
+
+def pedir_letra():
+    abecedario ='abcdfghikalmnñopqrstuvwxyz'
+    letra=input("Ingrease una letra al juego:").lower()
+    if len(letra) !=1 or letra not in abbecedario: 
+        print("Ingresa solo letras.")
+        return pedir_letra()
+    return letra 
+#no te olvides de gurdar los cambios, sino no carga
